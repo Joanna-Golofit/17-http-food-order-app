b64e817049e35ffa10220630aace62235aa10311
Joanna Gołofit <jgolofit@future-processing.com>
2022-11-17 16:53:23 +0100

    my version of -> Adding Better User Feedback

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,6 +9,8 @@ import Checkout from './Checkout';
 const Cart = (props) => {
 
   const [isCheckout, setIsCheckout] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [didSubmit, setDidSubmit] = useState(false)
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -26,15 +28,19 @@ const Cart = (props) => {
     setIsCheckout(true)
   }
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://http-project-134be-default-rtdb.firebaseio.com/orders.json', {
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch('https://http-project-134be-default-rtdb.firebaseio.com/orders.json', {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
         orderedItems:cartCtx.items
       })
     })
+    setIsSubmitting(false);
+    setDidSubmit(true);
   }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -57,8 +63,8 @@ const Cart = (props) => {
     {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
   </div>;
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent =
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -66,6 +72,17 @@ const Cart = (props) => {
       </div>
       {isCheckout && <Checkout onConfirm={submitOrderHandler} onClose={props.onClose} />}
       {!isCheckout && modalActions}
+    </>;
+  
+  const confirmationContent = <p>sent!</p>
+  const sendingContent = <p>sending.....</p>
+
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!didSubmit && !isSubmitting && cartModalContent}
+      {!didSubmit && isSubmitting && sendingContent}
+      {didSubmit && confirmationContent}
     </Modal>
   );
 };
